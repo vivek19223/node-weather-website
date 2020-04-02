67bb0d90882e7845f248ec5e390ba25d58fd06bd
vivek19223 <vivek19223@gmail.com>
2020-04-02 21:20:30 +0530

    Removed Unnecessery comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('.input');
 const info_1 = document.querySelector('#message_1');
